757c3c97d69eeb282c1cd79aa72835cd998f692a
KilJaeeun <rha3122@naver.com>
2021-04-08 02:14:29 +0000

    feat: Review JDBC Respository 생성

---
+++ src/main/java/com/github/prgrms/orders/JdbcReviewRepository.java
@@ -0,0 +1,41 @@
+package com.github.prgrms.reivews;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.github.prgrms.utils.DateTimeUtils.dateTimeOf;
+import static java.util.Optional.ofNullable;
+
+@Repository
+public class JdbcReivewRepository implements ReivewRepository {
+
+  private final JdbcTemplate jdbcTemplate;
+
+  public JdbcReivewRepository(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  @Override
+  public Integer create(Reivew review) {
+    Integer seq= jdbcTemplate.update(
+      "INSERT INTO reviews( user_seq, product_seq, content, create_at)  VALUES(?,?,?,?) ",
+    review.getUserSeq(),review.getProductSeq(),review.getContent(),review.getCreateAt());
+    return seq;
+  }
+
+
+
+  static RowMapper<Reivew> mapper = (rs, rowNum) ->
+    new Reivew.Builder()
+      .seq(rs.getLong("seq"))
+      .userSeq(rs.getLong("user_seq"))
+      .productSeq(rs.getLong("product_seq"))
+      .content(rs.getString("content"))
+      .createAt(dateTimeOf(rs.getTimestamp("create_at")))
+      .build();
+
+}
