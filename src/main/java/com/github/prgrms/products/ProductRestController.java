ab08998b9171fcf76b5c8ba0e5fd90a659a5951a
KilJaeeun <rha3122@naver.com>
2021-04-08 00:45:20 +0000

    feat:ProductRestController findById return 타입 수정

--- src/main/java/com/github/prgrms/products/ProductRestController.java
+++ src/main/java/com/github/prgrms/products/ProductRestController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
-
+import static com.github.prgrms.utils.ApiUtils.ApiResult;
+import static com.github.prgrms.utils.ApiUtils.success;
 @RestController
 @RequestMapping("api/products")
 public class ProductRestController {
@@ -20,12 +21,12 @@ public class ProductRestController {
     this.productService = productService;
   }
 
-  // FIXME `요건 1` 정의에 맞게 응답 타입 수정이 필요합니다.
+
   @GetMapping(path = "{id}")
-  public ProductDto findById(@PathVariable Long id) {
-    return productService.findById(id)
+  public ApiResult<ProductDto> findById(@PathVariable Long id) {
+    return success(productService.findById(id)
       .map(ProductDto::new)
-      .orElseThrow(() -> new NotFoundException("Could not found product for " + id));
+      .orElseThrow(() -> new NotFoundException("Could not found product for " + id)));
   }
 
   // FIXME `요건 1` 정의에 맞게 응답 타입 수정이 필요합니다.
